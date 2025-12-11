d7baa646206950a6510a7d4429b6e91cf4208b5a
Linadij <115348608+Linadij@users.noreply.github.com>
2025-12-11 15:48:08 +0200

    Refactor Main class to use methods for baking steps

--- Main.java
+++ Main.java
@@ -3,12 +3,17 @@ package stunda2;
 public class Main {
 
     public static void main(String[] args) {
-        //create a batter
+        makeBatter();
+        bake();
+        decorate();
+    }
+    public static void  makeBatter() {
         System.out.println("Mix the dry ingredients.");
         System.out.println("Cream the butter and sugar.");
         System.out.println("Beat in the eggs.");
         System.out.println("Stir in the dry ingredients.");
-        //bake 
+    }
+    public static void bake() {
         System.out.println("Set the oven temperature.");
         System.out.println("Set the timer.");
         System.out.println("Place a batch of cookies into the oven.");
@@ -17,9 +22,9 @@ public class Main {
         System.out.println("Set the timer.");
         System.out.println("Place a batch of cookies into the oven.");
         System.out.println("Allow the cookies to bake.");
-        //decorate
+    }
+    public static void decorate() {
         System.out.println("Mix ingredients for frosting.");
         System.out.println("Spread frosting and sprinkles.");
     }
 }
-
